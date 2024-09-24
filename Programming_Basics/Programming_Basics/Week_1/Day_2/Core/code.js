b9f8e8c09e4db90f2c82dc49498447269f330d7a
Mohamed Aziz Tounsi <tounsiasiz@gmail.com>
2024-09-24 14:25:59 +0100

    commenting

---
+++ Programming_Basics/Programming_Basics/Week_1/Day_2/Core/code.js
@@ -0,0 +1,13 @@
+var i 
+var year=0
+for (i=1996 ; i <=2024 ;i++){
+    if (year==10){
+        console.log(i,"HAPPY decade of js");
+       }
+       return year ==0;
+
+
+   
+}
+
+    
---
+++ Programming_Basics/Programming_Basics/Week_1/Day_2/Optional/code.js
@@ -0,0 +1,7 @@
+// this variable have 0 - what would this variable do?
+var likeCount = 0;
+// count the likes - what would be the function of this code block?
+function increaseLikes() {
+    // add +1 to the like count after anny like in crease - what is this sort operation for?
+    likeCount = likeCount + 1;
+}
