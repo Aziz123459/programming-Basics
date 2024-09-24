bf769f0113ab6ec25256b2935c3567c649bfd4b0
Mohamed Aziz Tounsi <tounsiasiz@gmail.com>
2024-09-24 11:40:40 +0100

    create variables

---
+++ Programming_Basics/Programming_Basics/Week_1/Day_2/Core/Tounsi_Mohmed_Aziz_JS_Createvariables.js
@@ -0,0 +1,12 @@
+export function main(){
+    console.log("create a variabel");
+    //my code
+    var firstname="Mohamed Aziz"                       //string
+    console.log(firstname);                            //Mohamed Aziz
+    var age =20                               //number
+    var number=10                             //number
+    console.log(age + number);                   //30
+    var birth="1 february 2005"                 // numbers trated as a string between the quotes
+    var firstname=true;                        //boolean
+    console.log(firstname);                   //true
+}
