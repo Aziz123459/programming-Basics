638ba5168c4cdea3c9d7ef3f591a55bc04bd07ff
Mohamed Aziz Tounsi <tounsiasiz@gmail.com>
2024-09-24 14:54:07 +0100

    predict arrays

---
+++ Programming_Basics/Programming_Basics/Week_1/Day_2/Core/predict arrays.js
@@ -0,0 +1,14 @@
+//result(paula ,smith ,1234 main street ,st.Louis, Mo ,12345)
+var contactInfo = ["Paula", "Smith", "1234 Main Street", "St. Louis", "MO", 12345];
+console.log(contactInfo);
+//result(broccoli,ice cream,hashbrowns)
+var produce = ["apples", "oranges"];
+var frozen = ["broccoli", "ice cream"];
+frozen.push("hashbrowns");
+console.log(frozen);
+//result(bambi,beetlejuice,toy story,zorro)
+var movieLibrary = ["Bambi", "E.T.", "Toy Story"];
+movieLibrary.push("Zorro");
+movieLibrary[1] = "Beetlejuice";
+console.log(movieLibrary);
+
