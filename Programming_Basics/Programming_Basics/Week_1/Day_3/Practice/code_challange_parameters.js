5a3d38b734cf99141cae63f65d5152ad4fa02c51
Mohamed Aziz Tounsi <tounsiasiz@gmail.com>
2024-09-25 11:42:24 +0100

    challenge parameters

---
+++ Programming_Basics/Programming_Basics/Week_1/Day_3/Practice/code_challange_parameters.js
@@ -0,0 +1,11 @@
+
+
+function greating (name,time){
+    if(name=="count dooku"){
+        console.log("i am coming for you dokku")
+    }
+    else{
+    console.log("good day " + name + " "+time)
+}
+}
+greating("aziz","8AM")
